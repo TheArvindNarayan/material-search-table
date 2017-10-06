be0ad0873c99d54dac9d47ce5b28b945480cefa2
arvind <thearvindnarayan@gmail.com>
2017-10-06 06:12:48 -0700

    checkbox sync and count

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
     
     // Write your custom Javascript codes here...
-    
+       $('#check-all').click(function() {console.log("ph-1");
+        if ($(this).is(':checked')) {
+            $('.check').prop('checked', true);
+        } else {
+            $('.check').prop('checked', false);
+        }
+    });
+
+
+
 });
